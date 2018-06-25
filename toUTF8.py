e6f2db480da8348d7e646a88ff4a6dc4e17c12e4
icwells <smrupp16@gmail.com>
2018-06-25 11:28:53 -0700

    Fixed keyword error with codecs.decode and now prints number of lines converted

--- toUTF8.py
+++ toUTF8.py
@@ -8,29 +8,39 @@ from argparse import ArgumentParser
 
 def recode(infile, outfile):
 	# Writes contents of infile to outfile in UTF-8. Encoding is determined line-by-line
+	passes = 0
+	total = 0
 	print(("\n\tDetermining encoding for each line in {}...").format(os.path.split(infile)[1]))
 	print(("\tWriting UTF-8 encoded data to {}...").format(os.path.split(outfile)[1]))
 	with codecs.open(outfile, "w", encoding="utf-8") as output:
 		with open(infile, "rb") as f:
 			for line in f:
+				total += 1
 				try:
 					enc = chardet.detect(line)
-					s = codecs.decode(line, encoding=enc["encoding"])
+					s = codecs.decode(line, enc["encoding"])
 					output.write(s)
-				except (UnicodeDecodeError, TypeError):
+					passes += 1
+				except UnicodeDecodeError:
 					pass
+	return passes, total
 
 def convert(infile, outfile, enc):
 	# Writes contents of infile to outfile in UTF-8 usning single input encoding
+	passes = 0
+	total = 0
 	print(("\n\tReading {} with {} encoding...").format(os.path.split(args.i)[1], args.e))
 	print(("\tWriting UTF-8 encoded data to {}...").format(os.path.split(outfile)[1]))
 	with codecs.open(outfile, "w", encoding="utf-8") as output:
 		with codecs.open(infile, "r", encoding=enc) as f:
 			for line in f:
+				total += 1
 				try:
 					output.write(line[:-1] + "\r\n")
+					passes += 1
 				except UnicodeDecodeError:
 					pass
+	return passes, total
 
 def main():
 	start = datetime.now()
@@ -47,9 +57,10 @@ help = "Path to input file. Output will be written in the same directory.")
 		quit()
 	outfile = ("{}.UTF{}").format(args.i[:args.i.find(".")], args.i[args.i.rfind("."):])
 	if args.e:
-		convert(args.i, outfile)
+		p, t = convert(args.i, outfile)
 	else:
-		recode(args.i, outfile)
+		p, t = recode(args.i, outfile)
+	print(("\tConverted {} out of {} total lines.").format(p, t))
 	print(("\tFinished. Runtime: {}\n").format(datetime.now()-start))
 
 if __name__ == "__main__":
