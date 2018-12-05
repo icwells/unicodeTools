f7bd20f89a6eda0fcde17c3da533a5427435e46a
icwells <smrupp16@gmail.com>
2018-12-05 11:18:27 -0700

    Merged toCSV and toUTF8

--- test_unicodeTools.py
+++ test_unicodeTools.py
@@ -1,3 +1,11 @@
-'''Performs black box tests on unicode tools scripts.'''
+'''Performs black and white box tests on unicode tools scripts.'''
 
 import pytest
+from toUTF8 import getDelim
+
+def test_getDelim():
+	# White box test of getDelim function
+	lines = [("first second\n", " "), ("first  \tsec,ond\n", "\t"), ("fir,st sec,ond\n", ",")]
+	for i in lines:
+		assert getDelim(i[0]) == i[1]
+
--- toUTF8.py
+++ toUTF8.py
@@ -6,10 +6,31 @@ import chardet
 from datetime import datetime
 from argparse import ArgumentParser
 
-def recode(infile, outfile):
+def getDelim(line):
+	# Returns delimiter
+	for i in ["\t", ",", " "]:
+		if i in line:
+			return i
+	print("\n\t[Error] Cannot determine delimeter. Check file formatting. Exiting.\n")
+	quit()
+
+def reformat(line, delim, csv):
+	# Converts input line to csv/tsv
+	if csv == True:
+		sep = ","
+		rep = ";"
+	else:
+		sep = "\t"
+		rep = " "
+	# Replace existing seperators
+	line = line.replace(sep, rep)
+	return sep.join(line.split(delim))
+
+def convert(infile, outfile, csv, tsv):
 	# Writes contents of infile to outfile in UTF-8. Encoding is determined line-by-line
 	passes = 0
 	total = 0
+	first = True
 	print(("\n\tDetermining encoding for each line in {}...").format(os.path.split(infile)[1]))
 	print(("\tWriting UTF-8 encoded data to {}...").format(os.path.split(outfile)[1]))
 	with codecs.open(outfile, "w", encoding="utf-8") as output:
@@ -19,47 +40,45 @@ def recode(infile, outfile):
 				try:
 					enc = chardet.detect(line)
 					s = codecs.decode(line, enc["encoding"])
+					if csv == True or tsv == True:
+						if first == True:
+							delim = getDelim(s)
+							first = False
+						s = reformat(s, delim, csv)
 					output.write(s)
 					passes += 1
 				except UnicodeDecodeError:
 					pass
 	return passes, total
 
-def convert(infile, outfile, enc):
-	# Writes contents of infile to outfile in UTF-8 usning single input encoding
-	passes = 0
-	total = 0
-	print(("\n\tReading {} with {} encoding...").format(os.path.split(args.i)[1], args.e))
-	print(("\tWriting UTF-8 encoded data to {}...").format(os.path.split(outfile)[1]))
-	with codecs.open(outfile, "w", encoding="utf-8") as output:
-		with codecs.open(infile, "r", encoding=enc) as f:
-			for line in f:
-				total += 1
-				try:
-					output.write(line[:-1] + "\r\n")
-					passes += 1
-				except UnicodeDecodeError:
-					pass
-	return passes, total
+def checkArgs(args):
+	# Checks input arguments for errors
+	if args.tsv and args.csv:
+		print("\n\t[Error] Please specify no more than one of csv/tsv. Exiting.")
+		quit()		
+	if not os.path.isfile(args.i):
+		print(("\n\t[Error] Cannot find {}. Exiting.").format(args.i))
+		quit()
+	path = args.i[:args.i.find(".")]
+	ext = args.i[args.i.rfind("."):]
+	if args.csv == True:
+		ext = ".csv"
+	elif args.tsv == True:
+		ext = ".tsv"
+	return args, ("{}.UTF8{}").format(path, ext)
 
 def main():
 	start = datetime.now()
 	parser = ArgumentParser("This script will convert an input file's \
-encoding to utf-8. All other formatting will remain unchanged.")
-	parser.add_argument("-e", 
-help = "Encoding of input file if known (otherwise encoding is determined on the fly).")
+encoding to utf-8.")
+	parser.add_argument("--csv", action = "store_true", default = False,
+help = "Optionally convert to comma seperated text files.")
+	parser.add_argument("--tsv", action = "store_true", default = False,
+help = "Optionally convert to tab seperated text files.")
 	parser.add_argument("i", 
 help = "Path to input file. Output will be written in the same directory.")
-	args = parser.parse_args()
-	# Check infile and get outfile
-	if not os.path.isfile(args.i):
-		print(("\n\t[Error] Cannot find {}. Exiting.").format(args.i))
-		quit()
-	outfile = ("{}.UTF{}").format(args.i[:args.i.find(".")], args.i[args.i.rfind("."):])
-	if args.e:
-		p, t = convert(args.i, outfile)
-	else:
-		p, t = recode(args.i, outfile)
+	args, outfile = checkArgs(parser.parse_args())
+	p, t = convert(args.i, outfile, args.csv, args.tsv)
 	print(("\tConverted {} out of {} total lines.").format(p, t))
 	print(("\tFinished. Runtime: {}\n").format(datetime.now()-start))
 
